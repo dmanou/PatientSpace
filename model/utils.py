49c00e36bd6d4be0eb5d5cbdca47fec84fc97882
dmanou <75978474+dmanou@users.noreply.github.com>
2026-05-12 14:38:34 +0200

    Add utility functions

---
+++ model/utils.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+import nibabel as nib
+
+def KL_DIV(q_mu, q_logvar, p_mu, p_logvar):
+    return 0.5 * torch.sum(p_logvar - q_logvar - 1 + (q_mu - p_mu)**2 / (p_logvar.exp() + 1e-5) + q_logvar.exp() / (p_logvar.exp() + 1e-5), dim =-1)
+
+def KL_DIV_normal(mu, logvar):
+    return -0.5 * torch.sum(1 + logvar - logvar.exp() - mu**2, dim =-1)
+
+def log_prob(x, x_rec):
+    return torch.sum((x.flatten(1) - x_rec.flatten(1)) ** 2, dim =-1)
+
+def decode(path):
+    x = nib.load(path).get_fdata()
+    x = np.nan_to_num(x)
+    mask = x > 0.
+    mean = (x * mask).sum() / mask.sum()
+    std = np.sqrt(((x * mask)**2).sum() / mask.sum() - mean**2)
+    zscore_img = (x - mean * mask) / std
+    zscore_img = zscore_img[10:170, 16:208, :160]
+    zscore_img = np.expand_dims(zscore_img, axis = 0)
+    zscore_img = torch.tensor(zscore_img, dtype = torch.float32)
+    return zscore_img
